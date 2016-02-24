67ab111548841973b52e4c39addadb1ffc855430
Jacopo Bellati <bellati@gmail.com>
2016-02-24 08:31:35 -0300

    added user_events permission to facebook scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-  :scope => 'public_profile', :display => 'popup',
+  :scope => 'public_profile, user_events,', :display => 'popup',
   :client_options => {
  	:site => 'https://graph.facebook.com/v2.5',
   	:authorize_url => "https://www.facebook.com/v2.5/dialog/oauth"

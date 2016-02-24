21ecd97327734d224fe784b50936cb143da8b3d2
Jacopo Bellati <bellati@gmail.com>
2016-02-24 08:46:11 -0300

    removed display option from omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-  :scope => 'public_profile, user_events,', :display => 'popup',
+  :scope => 'public_profile, user_events,',
   :client_options => {
  	:site => 'https://graph.facebook.com/v2.5',
   	:authorize_url => "https://www.facebook.com/v2.5/dialog/oauth"

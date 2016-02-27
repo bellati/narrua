6a0b07403c07c1e8294f15cc4dbbdc39a36b445f
Jacopo Bellati <bellati@gmail.com>
2016-02-27 09:03:56 -0300

    changed html_safe to sanitize - this allows only br tags

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Narrua
   class Application < Rails::Application
+    config.action_view.sanitized_allowed_tags = ['br']
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

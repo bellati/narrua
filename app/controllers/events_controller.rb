31d819c576a676b1d848f57d8baaae62cf5f0cf2
Jacopo Bellati <bellati@gmail.com>
2016-03-07 10:42:15 -0300

    added support for 'today', 'week' and 'month' events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,15 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all_from_today
+    if params[:time_frame].nil? || params[:time_frame] == '0' then
+      @events = Event.all_from_today
+    elsif params[:time_frame] == '1' then
+      @events = Event.all_from_current_week
+    elsif params[:time_frame] == '2' then
+      @events = Event.all_from_next_30_days
+    else
+      # TODO: redirect_to 404
+      @events = Event.all_from_today
+    end
   end
 
   def show

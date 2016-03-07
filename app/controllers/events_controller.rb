f887c828395e6ea4901da6a3fcce9b170d638125
Jacopo Bellati <bellati@gmail.com>
2016-03-07 10:54:22 -0300

    added support for multiple days events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,13 +2,17 @@ class EventsController < ApplicationController
   def index
     if params[:time_frame].nil? || params[:time_frame] == '0' then
       @events = Event.all_from_today
+      @context = 'HOJE'
     elsif params[:time_frame] == '1' then
       @events = Event.all_from_current_week
+      @context = 'SEMANA'
     elsif params[:time_frame] == '2' then
       @events = Event.all_from_next_30_days
+      @context = 'MÊS'
     else
       # TODO: redirect_to 404
       @events = Event.all_from_today
+      @context = 'HOJE'
     end
   end
 
